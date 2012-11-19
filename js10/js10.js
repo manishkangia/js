a4e71fca84e15744ca383593a48f366299920109
ShubhamGupta <shubham.gupta@vinsol.com>
2012-11-19 10:54:21 +0530

    jsexercise10

---
+++ js10/js10.js
@@ -0,0 +1,15 @@
+function checkField() {
+    var inputEle=document.getElementById("inputElement").value;
+    var putEle=document.getElementById("putElement");
+    var regex = /^\d+$/;
+    var check=regex.test(inputEle);
+    if(check) {
+       putEle.value="true";
+       alert("form submitted");
+    }
+    else {
+    putEle.value="false";
+    alert("form not submitted");
+    return false;
+    }
+}
