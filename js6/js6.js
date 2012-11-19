75e3b3e193b5baef8ed7a2fb92d0fc71fb70ae15
ShubhamGupta <shubham.gupta@vinsol.com>
2012-11-19 16:45:40 +0530

    js4-final

--- js6/js6.js
+++ js6/js6.js
@@ -1,24 +1,24 @@
-var fName,lName,toMatch=/[^ ]/g;
+var fName, lName, toMatch = /[^ ]/g;
 function getFirstName() {
-    fName = prompt("What is your first name? ", "");
-    if(fName.match(toMatch)!=null) {
-        fName=fName.trim();
-        getLastName()
+    fName = prompt("What is your first name?", "");
+    if (fName.match(toMatch) != null) {
+        fName = fName.trim();
+        getLastName();
     }
     else getFirstName();
 }
 
 function getLastName() {
-    lName= prompt("what is your last name?","");
-    if(lName.match(toMatch)!=null) {
-        lName=lName.trim();
-        alert("welcome "+ fName+" " + lName); 
-        initialDisplay(fName+" "+lName);
-        }
+    lName = prompt("what is your last name?", "");
+    if (lName.match(toMatch) != null) {
+        lName = lName.trim();
+        alert("welcome " + fName + " " + lName); 
+        initialDisplay(fName + " " +lName);
+    }
     else getLastName();
-}   
+}
 
 function initialDisplay(name) {
-    var para= document.getElementById("displayName");
-    para.innerHTML=name;
-}       
+    var para = document.getElementById("displayName");
+    para.innerHTML = name;
+}
