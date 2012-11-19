9309e256ccc2059f54f0742cc8fc30fa55b0b0f7
ShubhamGupta <shubham.gupta@vinsol.com>
2012-11-19 15:23:24 +0530

    jsexercise4-redone

--- js4/dynamicCheckBoxes.js
+++ js4/dynamicCheckBoxes.js
@@ -4,16 +4,21 @@ function dispChildList(callingNode) {
     var isChecked=callingNode.checked;
     var list= document.getElementsByClassName(groupName);
     
+    //Get all sub_checkboxes in same state as the checkbox
+    for(var i = 1;i < list.length; i++)
+        list[i].checked = true;
+        
     if(isChecked){
         list[1].style.display="";
-        getFocus(callingNode);
     }
     else{
         list[1].style.display = "none";
     }
-    
+    //Shift focus to selected checkbox
+    getFocus(callingNode);
 }
 
+//To get the window to focus on the selected node/checkbox
 function getFocus(focusNode){
     var div_ele= document.getElementsByClassName("container")[0];
     div_ele.scrollTop= focusNode.offsetTop -10;
