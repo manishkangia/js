0a97efef8cc8d5b9d79fd5cec8bef4850e5da261
ShubhamGupta <shubham.gupta@vinsol.com>
2012-11-19 17:09:47 +0530

    js7-final

--- js7/js7.js
+++ js7/js7.js
@@ -1,9 +1,9 @@
 function newWindow() {
-    console.log("called");
+    
     var urlentered=document.getElementById("url").value;
     var toMatch=/[^ ]/;
     if(toMatch.test(urlentered)) {
-    console.log(url);
+    
     myWindow=window.open(urlentered,'new window','width=400px','height=450px','status=no','scrollbars=no','toolbar=yes');
     }
     else {
