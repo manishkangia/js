edd8f54cb9f5d62eb4ccfb6a58bcf079feb1a09b
ShubhamGupta <shubham.gupta@vinsol.com>
2012-11-19 18:03:16 +0530

    js8-final

--- js8/js8.js
+++ js8/js8.js
@@ -1,21 +1,20 @@
-var move_list=new Array();
-//Function to create a list of selected indexes
-function generateList(selectedElement) {
-   console.log(move_list);
-   move_list= move_list + selectedElement.selectedIndex;
-  }
-  
-//function of shift the elements 
-//flow decides the flow of elements  
+
+//function of shift the elements   
 function move(from,to) {
    
    firstList = document.getElementById(from);
    secondList = document.getElementById(to);
-     
-   for(var i=0; i<move_list.length; i++)
+   
+   var move_list=new Array();  
+   for(var i=0; i<firstList.length; i++)
    {
-    var data= firstList.children[move_list[i]];
-    secondList.appendChild(data);
+    if(firstList[i].selected==true)
+    move_list = move_list + i;
+   }
+   
+   for(var i=0; i<move_list.length; i++)
+   { 
+    secondList.appendChild(firstList[move_list[move_list.length-1-i]]);
    }
    
    //to empty the array list for next action
