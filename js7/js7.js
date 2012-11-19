0950615511c042d12b250418a6f4fef98d92ee54
ShubhamGupta <shubham.gupta@vinsol.com>
2012-11-19 17:17:28 +0530

    js7-final

--- js7/js7.js
+++ js7/js7.js
@@ -1,10 +1,10 @@
 function newWindow() {
     
-    var urlentered=document.getElementById("url").value;
+    var url=document.getElementById("url").value;
     var toMatch=/[^ ]/;
-    if(toMatch.test(urlentered)) {
-    
-    myWindow=window.open(urlentered,'new window','width=400px','height=450px','status=no','scrollbars=no','toolbar=yes');
+    if(toMatch.test(url)) {
+    console.log(url);
+    myWindow=window.open(url,'new window','width=400px','height=450px','status=no','scrollbars=no','toolbar=yes');
     }
     else {
     alert("please enter a valid URl"); }
